e07b5de8a3dae20b3af974145895f0c627e43490
maccimac <macci.web@gmail.com>
2023-02-14 12:11:11 -0800

    Use list view

--- app/src/main/java/com/example/midtermpractice/CarRentalActivity.java
+++ app/src/main/java/com/example/midtermpractice/CarRentalActivity.java
@@ -8,28 +8,48 @@ import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 import java.lang.reflect.Field;
 import java.sql.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 
 public class CarRentalActivity extends AppCompatActivity {
 
     HashMap<String, String>[] companies;
+    ArrayList<HashMap<String, String>> compArrList = new ArrayList<>();
     String[] companiesStringArr = {"hertz", "enterprise"};
+    RecyclerView recyclerCompanies;
+    ListView listCompanies;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_car_rental);
 
-        RecyclerView recyclerCompanies = findViewById(R.id.recyclerCompanies);
+        recyclerCompanies = findViewById(R.id.recyclerCompanies);
+        listCompanies = findViewById(R.id.listviewCompanies);
+
         int columnCount = 1;
         recyclerCompanies.setLayoutManager(new GridLayoutManager(this, columnCount));
         companies = prepareCompanies(companiesStringArr);
+
         CarRentalAdapter adapter = new CarRentalAdapter(this, companies );
         recyclerCompanies.setAdapter(adapter);
+        setupListView(compArrList);
+    }
+
+    public void setupListView(ArrayList<HashMap<String, String>> arrList){
+
+        String[] fromHashKeys = {"name", "logoId"};
+        int[] toElements = {R.id.labelCompName, R.id.imgCompanyLogoList};
+
+        SimpleAdapter adapter = new SimpleAdapter(CarRentalActivity.this, arrList, R.layout.listview_car_company, fromHashKeys, toElements );
+        listCompanies.setAdapter(adapter);
+
     }
 
     public HashMap<String , String>[] prepareCompanies (String[] companiesArr){
@@ -49,6 +69,8 @@ public class CarRentalActivity extends AppCompatActivity {
             singleHash.put("logoId",  Integer.toString(drawableId));
             singleHash.put("url",  "http://google.com");
             companiesHash[i] = singleHash;
+
+            compArrList.add(singleHash);
         }
 
         System.out.println(Arrays.asList(companiesHash));
